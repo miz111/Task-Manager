5010e342394a032540551f3d0ff1635edcc27755
Minting <miz111@ucsd.edu>
2022-10-24 17:26:50 -0700

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
@@ -9,3 +9,11 @@ def project_list(request):
         "projects": projects,
     }
     return render(request, "projects/list.html", context)
+
+@login_required
+def show_project(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    context = {
+        "project_object": project,
+    }
+    return render(request, "projects/detail.html", context)
